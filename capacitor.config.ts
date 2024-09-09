53e08a2fda4d995281260bc40b305235db066c50
UTI305 <UTI305@sio.local>
2024-09-09 17:20:33 +0200

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.quartierprive',
+  appName: 'quartierprive',
+  webDir: 'www'
+};
+
+export default config;
